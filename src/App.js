fed441a6deaf6252e030444b5d5b5ae1773285b6
Shadman Khan <shaad2209@gmail.com>
2023-06-30 11:08:41 +0530

    updatde

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './Component/calculator';
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React Calculator</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
